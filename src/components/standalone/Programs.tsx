feaf464aa5a6ab3ed71be6a3d0e525c2670e6fd0
emmeiwhite <emmeiwhite@gmail.com>
2025-08-31 12:33:16 +0530

    Scale the card!

--- src/components/standalone/Programs.tsx
+++ src/components/standalone/Programs.tsx
@@ -37,7 +37,12 @@ export default function Programs() {
               return (
                 <motion.div
                   key={program.title}
-                  className="group cursor-pointer">
+                  className="group cursor-pointer"
+                  initial={{ opacity: 0, y: 24 }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  viewport={{ once: true }}
+                  transition={{ duration: 0.6, ease: 'easeInOut', delay: 0.2 * index }}
+                  whileHover={{ scale: 1.02 }}>
                   {/* Card Element from ShadCN */}
                   <Card className="p-0 transition-shadow duration-300 hover:shadow-2xl">
                     <div className="relative overflow-hidden rounded-t-lg w-full h-60 md:h-56 lg:h-40">

ff5e1366185ede38b6861b57dbefdeb436e53cab
emmeiwhite <emmeiwhite@gmail.com>
2025-09-15 12:24:57 +0530

    Brand section for footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,3 +1,6 @@
+import { Facebook, Instagram, Youtube } from 'lucide-react'
+import Link from 'next/link'
+
 export default function Footer() {
   return (
     <footer className="bg-secondary-foreground text-neutral-100 transition-colors duration-300">
@@ -5,7 +8,30 @@ export default function Footer() {
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           {/* Brand */}
           <div className="col-span-2">
-            <p className="text-primary text-2xl font-semibold">FitPro Fitness</p>
+            <p className="text-primary text-3xl font-black font-heading mb-4">FitPro Fitness</p>
+
+            <p className="mb-6 leading-relaxed max-w-md">
+              Transform your life with our comprehensive fitness program, expert trainers and
+              supportive community
+            </p>
+
+            <div className="flex gap-8 items-center">
+              <Link
+                href="https://instagram.com"
+                className="h-10 w-10 bg-secondary-foreground/40 rounded-full transition-colors duration-300 flex items-center justify-center hover:bg-primary ">
+                <Instagram className="w-5 h-5" />
+              </Link>
+              <Link
+                href="https://facebook.com"
+                className="h-10 w-10 bg-secondary-foreground/40 rounded-full transition-colors duration-300 flex items-center justify-center hover:bg-primary ">
+                <Facebook className="w-5 h-5" />
+              </Link>
+              <Link
+                href="https://youtube.com"
+                className="h-10 w-10 bg-secondary-foreground/40 rounded-full transition-colors duration-300 flex items-center justify-center hover:bg-primary ">
+                <Youtube className="w-5 h-5" />
+              </Link>
+            </div>
           </div>
 
           {/*  */}

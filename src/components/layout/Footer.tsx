79a90088b8e9fb8dc27aef2dce1c34ee823542e8
emmeiwhite <emmeiwhite@gmail.com>
2025-09-15 12:12:24 +0530

    Layout for footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,9 +1,16 @@
 export default function Footer() {
   return (
-    <footer className="bg-secondary-foreground text-primary-foreground py-6">
-      <p className="text-center">
-        &copy; 2025 <span className="text-primary">ProFit Fitness</span>
-      </p>
+    <footer className="bg-secondary-foreground text-neutral-100 transition-colors duration-300">
+      <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8 ">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+          {/* Brand */}
+          <div className="col-span-2">
+            <p className="text-primary text-2xl font-semibold">FitPro Fitness</p>
+          </div>
+
+          {/*  */}
+        </div>
+      </div>
     </footer>
   )
 }
